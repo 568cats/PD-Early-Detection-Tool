493f86dc4d4943dc18ecc494041dd7c6d732af09
Catherine S <81055937+568cats@users.noreply.github.com>
2023-08-03 12:11:33 -0400

    added ADaudio1model.py

---
+++ ADaudio1model.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+import scipy.io.wavfile
+#from IPython.display import Audio
+import tensorflow as tf
+from sklearn import metrics
+import librosa, librosa.display
+import pandas as pd
+import parselmouth
+import pickle
+
+def preprocess_ad_audio(path):
+    (sample_rate, signal) = scipy.io.wavfile.read(path)
+
+    sound = parselmouth.Sound(path)
+    pp = parselmouth.praat.call(sound, "To PointProcess (periodic, cc)...", 1, 200)
+    jitter = parselmouth.praat.call(pp, "Get jitter (ddp)", 0, 0, 0.0001, 0.02, 1.3)
+    shimmer = parselmouth.praat.call([sound, pp], "Get shimmer (local)", 0, 0, 0.0001, 0.2, 1.3, 1.6)
+
+    X_features = [jitter, shimmer]
+    return X_features
+
+
+def ad_audio_model():
+    with open("ADmodel2.pkl", "rb") as file:
+        model = pickle.load(file) 
+    return model
