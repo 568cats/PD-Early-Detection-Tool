49b37fa41a425e561a221a4254a1c39c6afc2784
Catherine S <81055937+568cats@users.noreply.github.com>
2023-08-04 10:08:46 -0400

    Update ADaudio1model.py

--- ADaudio1model.py
+++ ADaudio1model.py
@@ -14,10 +14,13 @@ def preprocess_ad_audio(path):
 
     sound = parselmouth.Sound(path)
     pp = parselmouth.praat.call(sound, "To PointProcess (periodic, cc)...", 1, 200)
-    jitter = parselmouth.praat.call(pp, "Get jitter (ddp)", 0, 0, 0.0001, 0.02, 1.3)
-    shimmer = parselmouth.praat.call([sound, pp], "Get shimmer (local)", 0, 0, 0.0001, 0.2, 1.3, 1.6)
+    #jitter = parselmouth.praat.call(pp, "Get jitter (ddp)", 0, 0, 0.0001, 0.02, 1.3)
+    #shimmer = parselmouth.praat.call([sound, pp], "Get shimmer (local)", 0, 0, 0.0001, 0.2, 1.3, 1.6)
+    num_unvoiced_frames = parselmouth.praat.call(pp, "Get number of periods", 0, 0, 0.0001, 0.02, 1.3)
+    total_frames = parselmouth.praat.call(pp, "Get number of points")
+    fraction_locally_unvoiced_frames = num_unvoiced_frames / total_frames if total_frames else 0
 
-    X_features = np.array([jitter, shimmer])
+    X_features = np.array([fraction_locally_unvoiced_frames)
     
     return X_features
 
